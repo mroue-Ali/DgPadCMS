e87086c09abb1d917bcdaddcddb40909527f7e60
MrouAli <mrou2001ali@gmail.com>
2022-09-03 18:27:25 +0300

    Edit PostType without checked taxonomy ( will do it after finish the most important tasks)

--- DgPadCMS/Areas/Admin/Controllers/PostTypeController.cs
+++ DgPadCMS/Areas/Admin/Controllers/PostTypeController.cs
@@ -70,7 +70,64 @@ namespace DgPadCMS.Areas.Admin.Controllers
             }
             return View(postTypeViewModel);
         }
-       
+        public async Task<IActionResult> Edit(int id)
+        {
+            var taxonomyList = await context.taxonomies.ToListAsync();
+            var postType = await context.postTypes.FirstOrDefaultAsync(x => x.Id == id);
+            PostTypeViewModel postTypeViewModel = new PostTypeViewModel();
+            postTypeViewModel.availabletaxonomies = taxonomyList;
+            postTypeViewModel.Title = postType.Title;
+            postTypeViewModel.Code = postType.Code;
+            return View(postTypeViewModel);
+
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(PostTypeViewModel postTypeViewModel, int id, List<int> taxonomyIdList)
+        {
+            var postType = await context.postTypes.FirstOrDefaultAsync(x => x.Id == id);
+            postType.Title = postTypeViewModel.Title;
+            postType.Code = postTypeViewModel.Code;
+            if (ModelState.IsValid)
+            {
+                var c = await context.postTypes.Where(x => x.Id != id).FirstOrDefaultAsync(x => x.Code == postTypeViewModel.Code);
+                if (c != null)
+                {
+                    ModelState.AddModelError("", "the post type alredy exists");
+
+
+                    return View(postTypeViewModel);
+
+                }
+
+                context.postTypes.Update(postType);
+                await context.SaveChangesAsync();
+                var ttt = await context.postTypeTaxonomies.Where(x => x.postTypeId == id).ToListAsync();
+                foreach (var item in ttt)
+                {
+                    context.postTypeTaxonomies.Remove(item);
+
+                }
+
+                await context.SaveChangesAsync();
+                foreach (var taxonomy in taxonomyIdList)
+                {
+                    PostTypeTaxonomy postTypeTaxonomy = new PostTypeTaxonomy()
+                    {
+                        taxonomyId = taxonomy,
+                        postTypeId = postType.Id,
+                    };
+
+                    context.Add(postTypeTaxonomy);
+                }
+                await context.SaveChangesAsync();
+                    
+                return RedirectToAction("Index");
+
+            }
+            return View(postTypeViewModel);
+        }
+
         public IActionResult Delete(PostType postType)
         {
             context.postTypes.Remove(postType);
