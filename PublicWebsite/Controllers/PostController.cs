9918f9d0c546756308bd8be19ac13848090ab5f1
MrouAli <mrou2001ali@gmail.com>
2022-09-20 01:02:50 +0300

    Adding tags and chaging favicon

--- PublicWebsite/Controllers/PostController.cs
+++ PublicWebsite/Controllers/PostController.cs
@@ -1,7 +1,10 @@
 ﻿using DgPadCMS.Infrastructure;
+using DgPadCMS.Models;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using PublicWebsite.Models;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -23,12 +26,29 @@ namespace PublicWebsite.Controllers
             ViewBag.PostType = postType.Title;
             return View(posts);
         }
+        public async Task<IActionResult> PostByTerm(int id)
+        {
+            var postterms = await context.postTerms.Include(x => x.Post).Where(x => x.TermId == id).ToListAsync();
+            var posts = new List<Post>();
+            ViewBag.Term = await context.terms.FindAsync(id);
+            foreach (var post in postterms)
+            {
+                posts.Add(post.Post);
+            }
+            return View(posts);
+        }
         public async Task<IActionResult> Detail(int id)
         {
-            ViewBag.terms = await context.posts.Include(x => x.postTerms).ThenInclude(x => x.Term).ToListAsync();
-           
+            var pt = await context.postTerms.Include(x => x.Term).Where(x=>x.PostId==id).ToListAsync();
+            var p=await context.posts.Include(x => x.postTerms).ThenInclude(x=>x.Term).ToListAsync();
+        
+
             var post = await context.posts.FindAsync(id);
-            return View(post);
+            DetailViewModel detailViewModel = new DetailViewModel();
+            detailViewModel.post = post;
+            detailViewModel.posts = p;
+            detailViewModel.postTerms = pt;
+            return View(detailViewModel);
         }
     }
 }
---
+++ PublicWebsite/Models/DetailViewModel.cs
@@ -0,0 +1,12 @@
+﻿using DgPadCMS.Models;
+using System.Collections.Generic;
+
+namespace PublicWebsite.Models
+{
+    public class DetailViewModel
+    {
+        public Post post { get; set; }
+        public List<Post> posts { get; set; }
+        public List<PostTerm> postTerms { get; set; }
+    }
+}
